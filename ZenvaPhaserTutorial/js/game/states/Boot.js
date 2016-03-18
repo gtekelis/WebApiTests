f93d2bbed824017500c4a63b2fa83a3daa1ac4b6
George Tekelis <tekelisg@gmail.com>
2016-03-18 16:15:28 +0200

    ok

---
+++ ZenvaPhaserTutorial/js/game/states/Boot.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="../../library/phaser.js" />
+var ZenvaRunner = function () { };
+
+ZenvaRunner.Boot = function () { };
+
+ZenvaRunner.Boot.prototype = {
+    preload: function () {
+        this.load.image('logo', 'assets/images/logo.png');
+        this.load.image('preloadbar', 'assets/images/preloader-bar.png');
+    },
+
+    create: function(){
+        this.game.stage.backGroundColor = '#fff';
+    }
+};
--- ZenvaPhaserTutorial/js/main.js
+++ ZenvaPhaserTutorial/js/main.js
@@ -1,3 +1,6 @@
 ﻿///creates an instance of our game
 /// <reference path="library/phaser.js" />
-var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '')
+var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '');
+
+game.state.add('Boot', ZenvaRunner.Boot);
+game.stage.start('Boot');
