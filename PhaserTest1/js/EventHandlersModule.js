3d41e3845c7981513868f88afb2d9a3595b4d56a
George Tekelis <tekelisg@gmail.com>
2016-03-21 17:28:45 +0200

    ok

---
+++ PhaserTest1/js/EventHandlersModule.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="C:\Users\gtekelis\Documents\Visual Studio 2015\Projects\WebApiTests\PhaserTest1\libs/phaser.js" />
+/// <reference path="ElectricSystemModule.js" />
+/// <reference path="C:\Users\gtekelis\Documents\Visual Studio 2015\Projects\WebApiTests\PhaserTest1\libs/jquery-1.12.2.min.js" />
+
+
+var eventHandlers = (function () {
+    console.log('init event handlers');
+        
+    //private members
+    function actionOnClick() {
+        $('body').addClass('animated hinge');
+        //alert('info here');
+    }
+    
+    //public members
+    return {
+        alertMessageOnClick: actionOnClick
+    }
+})();
---
+++ PhaserTest1/js/PhaserGameModule.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="C:\Users\gtekelis\Documents\Visual Studio 2015\Projects\WebApiTests\PhaserTest1\libs/phaser.js" />
+
+var phaserGame = (function () {
+    console.log('PhaserGame module initialized');
+
+    function createGame() {
+        return new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+    }
+
+    return {
+        CreateGame: createGame
+    }
+})();
